ecaf343fc85998342d4e61d6579592f3d3bb747b
Benoit Verret <benoit.verret@toumoro.com>
2020-07-17 16:22:16 -0400

    ascsdac

--- scripts/codebuild_helper.py
+++ scripts/codebuild_helper.py
@@ -39,11 +39,9 @@ def error_handler(code):
         print('NO FAIL')
 
 def registry_login(username, password, registry="docker.io"):
-    for i in username:
-        print(i)
     print("")
     print("Login to docker")
-    res, err = console_command(["echo", password, "|", "docker", "login", "-u", username, "--password-stdin", registry])
+    res, err = console_command(["docker", "login", "-u", username, "-p", password, registry])
     print(res)
     if err:
         raise AssertionError(f"{err}")
